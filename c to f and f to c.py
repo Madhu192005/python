0226e901093a65306982e0fbd3427e3248ea2b3e
Madhu192005 <147263453+Madhu192005@users.noreply.github.com>
2024-12-02 15:39:46 +0530

    Add files via upload

---
+++ c to f and f to c.py
@@ -0,0 +1,9 @@
+#celsius to fahrenheit
+celsius=int(input("Enter the degree : "))
+fahrenheit = (9/5)*celsius + 32
+print("The given degree is",fahrenheit,"degree")
+
+#fahrenheit to celsius
+fahrenheit=int(input("Enter the degree: "))
+celsius=(5/9)*(fahrenheit - 32)
+print("The given degree is",celsius,"degree")
